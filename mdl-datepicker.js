8623847b5dd106eb00962ac718bbb13c9af55025
trentsp <corcastus@gmail.com>
2015-08-10 20:23:38 -0400

    Fixed error when more than one datepicker is on page

--- mdl-datepicker.js
+++ mdl-datepicker.js
@@ -8,18 +8,18 @@ $.fn.mdldatepicker = function(){
   var month_end = moment().endOf('month');
 
   var theHTML =
-    '<div id="mdl-datepicker-div" tabindex="0" class="mdl-cell mdl-cell--2-col mdl-card mdl-shadow--2dp mdl-js-layout mdl-js-ripple-effect mdl-datepicker mdl-datepicker-hide">'+
+    '<div id="mdl-datepicker-div-' + this[0].id + '" class="mdl-cell mdl-cell--2-col mdl-card mdl-shadow--2dp mdl-js-layout mdl-js-ripple-effect mdl-datepicker mdl-datepicker-hide">'+
       '<div class="header-background mdl-datepicker">'+
-        '<div id="mdl-datepicker-year" class="mdl-typography--regular mdl-datepicker"></div>'+
-        '<div id="mdl-datepicker-selected-date" class="mdl-typography--headline mdl-datepicker" style="margin-left:1em;"></div>'+
+        '<div id="mdl-datepicker-year-' + this[0].id + '" class="mdl-typography--regular mdl-datepicker"></div>'+
+        '<div id="mdl-datepicker-selected-date-' + this[0].id + '" class="mdl-typography--headline mdl-datepicker" style="margin-left:1em;"></div>'+
       '</div>'+
-      '<div id="mdl-datepicker-calendar-header" class="mdl-datepicker" style="display:inline-block; text-align:center">'+
-        '<div id="mdl-datepicker-previous-month" class="mdl-datepicker" style="display:inline-block;float:left;position:relative;cursor:pointer;" onclick="previousMonth()"><i class="material-icons mdl-datepicker">keyboard_arrow_left</i></div>'+
-        '<div id="mdl-datepicker-current-viewed-month-year" class="mdl-datepicker" style="display:inline-block;margin-top:.15em"></div>'+
-        '<div id="mdl-datepicker-next-month" class="mdl-datepicker" style="display:inline-block;float:right;position:relative;cursor:pointer;" onclick = "nextMonth()"><i class="material-icons mdl-datepicker">keyboard_arrow_right</i></div>'+
+      '<div id="mdl-datepicker-calendar-header-' + this[0].id + '" class="mdl-datepicker" style="display:inline-block; text-align:center;">'+
+        '<div id="mdl-datepicker-previous-month-' + this[0].id + '" class="mdl-datepicker" style="display:inline-block;float:left;position:relative;cursor:pointer;" onclick="previousMonth(this);"><i class="material-icons mdl-datepicker">keyboard_arrow_left</i></div>'+
+        '<div id="mdl-datepicker-current-viewed-month-year-' + this[0].id + '" class="mdl-datepicker" style="display:inline-block;margin-top:.15em"></div>'+
+        '<div id="mdl-datepicker-next-month-' + this[0].id + '" class="mdl-datepicker" style="display:inline-block;float:right;position:relative;cursor:pointer;" onclick = "nextMonth(this);"><i class="material-icons mdl-datepicker">keyboard_arrow_right</i></div>'+
       '</div>'+
-      '<div id="mdl-datepicker-calendar-container" class="mdl-datepicker">'+
-        '<table id="mdl-datepicker-daysofweek" class="mdl-datepicker mdl-typography--regular mdl-datepicker-calendar">'+
+      '<div id="mdl-datepicker-calendar-container-' + this[0].id + '" class="mdl-datepicker">'+
+        '<table id="mdl-datepicker-daysofweek-' + this[0].id + '" class="mdl-datepicker mdl-typography--regular mdl-datepicker-calendar">'+
           '<tr>'+
             '<td class="mdl-datepicker">S</td>'+
             '<td class="mdl-datepicker">M</td>'+
@@ -30,11 +30,11 @@ $.fn.mdldatepicker = function(){
             '<td class="mdl-datepicker">S</td>'+
           '</tr>'+
         '</table>'+
-        '<table id="mdl-datepicker-calendar" class="mdl-typography--regular mdl-datepicker-calendar mdl-datepicker">'+
+        '<table id="mdl-datepicker-calendar-' + this[0].id + '" class="mdl-typography--regular mdl-datepicker-calendar mdl-datepicker">'+
         '</table>'+
-        '<div style="display:inline-block; float:right;">' +
-          '<button class="mdl-button mdl-js-button mdl-button--accent" style="position:relative;top:.5em" onclick="self.hideCalendar();">Cancel</button>' +
-          '<button class="mdl-button mdl-js-button mdl-button--primary" style="position:relative;top:.5em; right:0" onclick="self.insertDate();">OK</button>' +
+        '<div style="display:inline-block; position:absolute; top:85%;">' +
+          '<button class="mdl-button mdl-js-button mdl-button--accent" style="position:relative;top:.5em" onclick="hideCalendar();">Cancel</button>' +
+          '<button class="mdl-button mdl-js-button mdl-button--primary" style="position:relative;top:.5em; right:0" onclick="insertDate(' + this[0].id + ');">OK</button>' +
         '</div>' +
       '</div>'+
       '<span class="mdl-ripple" />'+
@@ -58,22 +58,32 @@ $.fn.mdldatepicker = function(){
   //
   //                    Locating the DatePicker
   //
+  /*
+  var fieldHeight = $(field).height();
+	var offsetTop = $(field).offset().top + fieldHeight + 10;
+	var offsetLeft = $(field).offset().left;
+  datePicker.css('position', 'relative');
+	datePicker.css('top', offsetTop);
+	datePicker.css('left', offsetLeft);
+  */
   datePicker.css('width', "13em");
   datePicker.css('height', "18em");
+  datePicker.css('z-index', "9999");
 
   //
   //                    Initializing
   //
 
-  $("#mdl-datepicker-year").text(selected_year);
-  $("#mdl-datepicker-selected-date").text(selected_date);
-  $("#mdl-datepicker-current-viewed-month-year").text(current_viewed_month_year);
+  $("#mdl-datepicker-year-" + field[0].id).text(selected_year);
+  $("#mdl-datepicker-selected-date-" + field[0].id).text(selected_date);
+  $("#mdl-datepicker-current-viewed-month-year-" + field[0].id).text(current_viewed_month_year);
   buildCalendar(month_start.format("ddd"), month_end.format("D"));
   //
   //                    Functions
   //
 
-  self.previousMonth = function(){
+  self.previousMonth = function(incoming){
+    var fieldId = incoming.id.substring(30);
     if (current_viewed_month == '1'){
       current_viewed_month = 12;
       var tempYear = Number(current_viewed_year);
@@ -85,14 +95,15 @@ $.fn.mdldatepicker = function(){
 
     var stringDate = current_viewed_year + "-" + current_viewed_month + "-01";
     current_viewed_month_year = moment(stringDate, ["YYYY-M-D"]).format("MMMM YYYY");
-    $("#mdl-datepicker-current-viewed-month-year").text(current_viewed_month_year);
+    $("#mdl-datepicker-current-viewed-month-year-" + fieldId).text(current_viewed_month_year);
     var newStart = moment(stringDate, ["YYYY-M-D"]).startOf('month');
     var newEnd = moment(stringDate, ["YYYY-M-D"]).endOf('month');
-    $("#mdl-datepicker-calendar").empty();
-    buildCalendar(newStart.format("ddd"), newEnd.format("D"));
+    $("#mdl-datepicker-calendar-" + fieldId).empty();
+    buildCalendar(newStart.format("ddd"), newEnd.format("D"), fieldId);
   }
 
-  self.nextMonth = function(){
+  self.nextMonth = function(incoming){
+    var fieldId = incoming.id.substring(26);
     if (current_viewed_month == '12'){
       current_viewed_month = 1;
       var tempYear = Number(current_viewed_year);
@@ -104,50 +115,50 @@ $.fn.mdldatepicker = function(){
 
     var stringDate = current_viewed_year + "-" + current_viewed_month + "-01";
     current_viewed_month_year = moment(stringDate, ["YYYY-M-D"]).format("MMMM YYYY");
-    $("#mdl-datepicker-current-viewed-month-year").text(current_viewed_month_year);
+    $("#mdl-datepicker-current-viewed-month-year-" + fieldId).text(current_viewed_month_year);
     var newStart = moment(stringDate, ["YYYY-M-D"]).startOf('month');
     var newEnd = moment(stringDate, ["YYYY-M-D"]).endOf('month');
-    $("#mdl-datepicker-calendar").empty();
-    buildCalendar(newStart.format("ddd"), newEnd.format("D"));
+    $("#mdl-datepicker-calendar-" + fieldId).empty();
+    buildCalendar(newStart.format("ddd"), newEnd.format("D"), fieldId);
   }
 
   self.hideCalendar = function(){
     datePicker.addClass("mdl-datepicker-hide");
   }
 
-  self.insertDate = function(){
-    var messyDate = $("#mdl-datepicker-selected-date").text() + " " +  $("#mdl-datepicker-year").text();
+  self.insertDate = function(incoming){
+    var messyDate = $("#mdl-datepicker-selected-date-" + field[0].id).text() + " " +  $("#mdl-datepicker-year" + field[0].id).text();
     var theDate = moment(messyDate, ["ddd, MMM D YYYY"]).format("MMMM DD, YYYY");
-    $(field).val(theDate).trigger("change");
+    $(incoming).val(theDate).parent("div").addClass("is-dirty");
     hideCalendar();
   }
 
-  function buildCalendar(firstDayOfWeek, lastDayOfMonth){
+  function buildCalendar(firstDayOfWeek, lastDayOfMonth, fieldId){
+    fieldId = typeof fieldId !== 'undefined' ? fieldId : field[0].id;
     var first_row = "";
     var remaining_rows = "";
     var first_day_second_week = 8;
-
     switch (firstDayOfWeek){
       case "Sun":
         first_row = "<tr>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
-                      "<td id='3' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
-                      "<td id='4' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
-                      "<td id='5' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
-                      "<td id='6' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>6</td>" +
-                      "<td id='7' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>7</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='3-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
+                      "<td id='4-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
+                      "<td id='5-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
+                      "<td id='6-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>6</td>" +
+                      "<td id='7-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>7</td>" +
                      "</tr>";
         break;
         case "Mon":
         first_row = "<tr>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
-                      "<td id='3' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
-                      "<td id='4' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
-                      "<td id='5' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
-                      "<td id='6' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>6</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='3-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
+                      "<td id='4-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
+                      "<td id='5-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
+                      "<td id='6-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>6</td>" +
                      "</tr>";
         first_day_second_week = 7;
         break;
@@ -155,11 +166,11 @@ $.fn.mdldatepicker = function(){
         first_row = "<tr>" +
                       "<td></td>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
-                      "<td id='3' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
-                      "<td id='4' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
-                      "<td id='5' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='3-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
+                      "<td id='4-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
+                      "<td id='5-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>5</td>" +
                      "</tr>";
         first_day_second_week = 6;
         break;
@@ -168,10 +179,10 @@ $.fn.mdldatepicker = function(){
                       "<td></td>" +
                       "<td></td>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
-                      "<td id='3' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
-                      "<td id='4' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='3-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
+                      "<td id='4-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>4</td>" +
                      "</tr>";
         first_day_second_week = 5;
         break;
@@ -181,9 +192,9 @@ $.fn.mdldatepicker = function(){
                       "<td></td>" +
                       "<td></td>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
-                      "<td id='3' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='3-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>3</td>" +
                      "</tr>";
         first_day_second_week = 4;
         break;
@@ -194,8 +205,8 @@ $.fn.mdldatepicker = function(){
                       "<td></td>" +
                       "<td></td>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
-                      "<td id='2' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='2-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>2</td>" +
                      "</tr>";
         first_day_second_week = 3;
         break;
@@ -207,7 +218,7 @@ $.fn.mdldatepicker = function(){
                       "<td></td>" +
                       "<td></td>" +
                       "<td></td>" +
-                      "<td id='1' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
+                      "<td id='1-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>1</td>" +
                      "</tr>";
         first_day_second_week = 2;
         break;
@@ -216,24 +227,25 @@ $.fn.mdldatepicker = function(){
     var tempRow = ""
     for (calendarLoop = first_day_second_week; calendarLoop <= lastDayOfMonth; calendarLoop++){
       if (dayCount < 7){
-        tempRow += "<td id='" + calendarLoop + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>" + calendarLoop + "</td>";
+        tempRow += "<td id='" + calendarLoop + "-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>" + calendarLoop + "</td>";
         dayCount++
       } else {
         remaining_rows += "<tr>" + tempRow + "</tr>";
-        tempRow = "<td id='" + calendarLoop + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>" + calendarLoop + "</td>";
+        tempRow = "<td id='" + calendarLoop + "-"  + fieldId + "' class='mdl-datepicker-clickable mdl-datepicker' style='cursor:pointer;'>" + calendarLoop + "</td>";
         dayCount = 1;
       }
     }
+
     remaining_rows += "<tr>" + tempRow + "</tr>";
-    $("#mdl-datepicker-calendar").append(first_row + " " + remaining_rows);
+    $("#mdl-datepicker-calendar-" + fieldId).append(first_row + " " + remaining_rows);
     if (current_viewed_month == moment().format("M")){
-      $("#" + moment().format("D")).addClass("mdl-datepicker-today");
+      $("#" + moment().format("D") + "-" + fieldId).addClass("mdl-datepicker-today");
     }
     $(".mdl-datepicker-clickable").click(function(incoming){
       var stringDate = current_viewed_year + "-" + current_viewed_month + "-" + incoming.currentTarget.id;
       selected_date = moment(stringDate, ["YYYY-M-D"]).format("ddd, MMM D")
-      $("#mdl-datepicker-year").text(current_viewed_year);
-      $("#mdl-datepicker-selected-date").text(selected_date);
+      $("#mdl-datepicker-year-" + fieldId).text(current_viewed_year);
+      $("#mdl-datepicker-selected-date-" + fieldId).text(selected_date);
     });
-  }
+  }//end of buildCalendar
 }
